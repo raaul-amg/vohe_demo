1b75df843531d1b4983d07d52ab687fcf4382a99
Raúl <raul.mgherman@alumnos.upm.es>
2026-04-20 02:47:22 +0200

    cambiado el estilo del login y empezado con el del public (tema y texto con datos del usuario)

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,64 +1,70 @@
-import { useEffect, useState } from 'react';
-import { useAuth } from '../config/Auth';
-import { socket } from '../config/socket'
+import { useEffect, useState } from "react";
+import { useAuth } from "../config/Auth";
+import { socket } from "../config/socket";
 
 export default function Login() {
-
   const { usuario, setUsuario } = useAuth();
 
-  const [usuarioTemp, setUsuarioTemp] = useState('');
-  const [password, setPassword] = useState('');
+  const [usuarioTemp, setUsuarioTemp] = useState("");
+  const [password, setPassword] = useState("");
 
   const enviar = (e) => {
-        e.preventDefault();
-        if (password.trim() === '') return;
+    e.preventDefault();
+    if (password.trim() === "") return;
 
-        const payload = {
-          usuario: usuarioTemp,
-          password: password
-        }
-
-        socket.emit('login', payload)
+    const payload = {
+      usuario: usuarioTemp,
+      password: password,
     };
 
-    useEffect(() => {
+    socket.emit("login", payload);
+  };
 
-      socket.on('resLogin', (datos) => {
+  useEffect(() => {
+    socket.on("resLogin", (datos) => {
+      if (!datos) {
+        alert("Usuario o contraseña incorrectos");
+        return;
+      }
 
-        if (!datos) {
-          alert("Usuario o contraseña incorrectos");
-          return;
-        }
+      localStorage.setItem("ceettoken", datos.token);
+      setUsuario(datos.userData);
+    });
 
-        localStorage.setItem('ceettoken', datos.token);
-        setUsuario(datos.userData);
-      })
+    return () => {
+      socket.off("resLogin");
+    };
+  }, [setUsuario]);
 
-      return () => {
-        socket.off('resLogin');
-      };
-          
-    }, [setUsuario]);
-
-    return (
-    <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-sm mx-auto my-auto">
-      <h2 className="text-ceet text-4xl font-ceet bg-clip-text mb-8 mx-auto">Inicio de sesión</h2>
-      
-      <form onSubmit={enviar}>
-        <input
+  return (
+    <div className="bg-ceet min-h-screen w-full flex justify-center items-center p-4">
+      <div className="bg-white p-8 rounded-xl shadow-lg flex flex-col gap-6 w-full max-w-md">
+        <h2 className="text-2xl text-ceet font-ceet font-bold text-center">
+          Inicio de sesión
+        </h2>
+        <form className="flex flex-col gap-3" onSubmit={enviar}>
+          <input
+            className="h-9 border border-ceet w-full text-ceet font-ceet text-center gap-5 rounded-md"
             type="text"
             placeholder="Usuario"
             value={usuarioTemp}
             onChange={(e) => setUsuarioTemp(e.target.value)}
-        />
-        <input 
-          type="password" 
-          placeholder="Contraseña" 
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Entrar</button>
-      </form>
+          />
+          <input
+            className="h-9 border border-ceet w-full text-ceet font-ceet text-center gap-5 rounded-md"
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="h-9 border border-ceet w-full bg-ceet text-white font-ceet text-center gap-5 rounded-md active:scale-95"
+            type="submit"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- frontend/src/views/Public.jsx
+++ frontend/src/views/Public.jsx
@@ -1,140 +1,191 @@
 import { useState, useEffect } from "react";
-import { useAuth } from '../config/Auth';
-import { socket } from '../config/socket'
+import { useAuth } from "../config/Auth";
+import { socket } from "../config/socket";
 
 export default function Public() {
-    
-    const { usuario, setUsuario } = useAuth();
+  const { usuario, setUsuario } = useAuth();
 
-    const [conectado, setConectado] = useState(false);
-    
-    const [asamblea, setAsamblea] = useState({
-      turnos: [],
-      historial: [],
-      tema: '',
-      turnoAbierto: true,
+  const [conectado, setConectado] = useState(false);
+
+  const [asamblea, setAsamblea] = useState({
+    turnos: [],
+    historial: [],
+    tema: "",
+    turnoAbierto: true,
+  });
+
+  const [tiempo, setTiempo] = useState(0);
+
+  useEffect(() => {
+    socket.on("estadoActualizado", (estado) => {
+      setAsamblea(estado);
+      setConectado(true);
     });
 
-    const [tiempo, setTiempo] = useState(0);
+    socket.on("tiempo", (t) => setTiempo(t));
 
-    useEffect(() => {
-    
-        socket.on('estadoActualizado', (estado) =>{
-          setAsamblea(estado)
-          setConectado(true);
-        });
-    
-        socket.on('tiempo', (t) => setTiempo(t));
+    socket.emit("pedirUpdate");
 
-        socket.emit('pedirUpdate')
-    
-        return () => {
-          socket.off('estadoActualizado');
-          socket.off('tiempo');
-        };
-    
-      }, []);
+    return () => {
+      socket.off("estadoActualizado");
+      socket.off("tiempo");
+    };
+  }, []);
 
-    const pedirTurno = (intervencion) => {
-        
-        const prioridades = {
-            'Apunte técnico': 5,
-            'Punto de información': 4,
-            'Respuesta por alusión directa': 3,
-            'Respuesta normal': 2,
-            'Intervención': 1,
-        };
+  const pedirTurno = (intervencion) => {
+    const prioridades = {
+      "Apunte técnico": 5,
+      "Punto de información": 4,
+      "Respuesta por alusión directa": 3,
+      "Respuesta normal": 2,
+      Intervención: 1,
+    };
 
-        socket.emit('pedirTurno', {
-            nombre: usuario.nombre,
-            delegacion: usuario.delegacion,
-            intervencion: intervencion,
-            prioridad: prioridades[intervencion],
-            solicitud: true
-        });
+    socket.emit("pedirTurno", {
+      nombre: usuario.nombre,
+      delegacion: usuario.delegacion,
+      intervencion: intervencion,
+      prioridad: prioridades[intervencion],
+      solicitud: true,
+    });
 
-        setNombre('');
-        setDelegacion('');
-
+    setNombre("");
+    setDelegacion("");
   };
 
   const cortarTurno = (datos) => {
+    socket.emit("cortarTurno", {
+      id: datos.id,
+      nombre: datos.nombre,
+      delegacion: datos.delegacion,
+    });
 
-        socket.emit('cortarTurno', {
-            id: datos.id,
-            nombre: datos.nombre,
-            delegacion: datos.delegacion,
-        });
-
-        setNombre('');
-        setDelegacion('');
-
+    setNombre("");
+    setDelegacion("");
   };
 
-    const formatTime = (seconds) => {
-        const mins = Math.floor(seconds / 60);
-        const secs = seconds % 60;
-        return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
-    };
+  const formatTime = (seconds) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
+  };
 
-    const cerrarSesion = (e) => {
-      e.preventDefault();
-      localStorage.removeItem("ceettoken");
-      setUsuario(null);
-    }
+  const cerrarSesion = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("ceettoken");
+    setUsuario(null);
+  };
 
-    return (
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center w-full gap-7">
+      <h1>Vista pública</h1>
+
       <div>
-        <h1>Vista pública</h1>
+        <span className="text-gray-400 font-bold font-ceet">
+          {usuario.rol !== null
+            ? `Has iniciado sesión como: ${usuario.nombre} - ${usuario.delegacion}`
+            : `Has iniciado sesión como: ${usuario.nombre} - ${usuario.delegacion} (${usuario.rol})`}
+        </span>
+      </div>
 
-        <div>
-          <h4>Quien soy</h4>
-          <ul>
-            <li>{usuario.nombre}</li>
-            <li>{usuario.delegacion}</li>
-            <li>{usuario.rol}</li>
-          </ul>
+      <div className="w-full py-4 flex flex-col justify-center overflow-hidden">
+        <div className="py-4 bg-ceet text-white">
+          <h2 className="gap-2 text-white font-ceet pl-4 animate-pulse">
+            Ahora hablando sobre...
+          </h2>
+          <div className="w-full overflow-hidden mt-1 @container gap-2">
+            <h3 className="text-4xl font-bold text-white font-ceet whitespace-nowrap inline-block animate-reveal w-max pl-4 pr-4">
+              {asamblea.tema || "Sin tema seleccionado"}
+            </h3>
+          </div>
         </div>
 
+        <h2 className="py-2 text-ceet font-ceet pl-4">
+          Descargar la documentación de este punto
+        </h2>
+      </div>
+
+      <br />
+
+      <div>
+        <h2>Turnos</h2>
+
         <div>
-          <h2>Tema</h2>
-          <h3>{asamblea.tema || "Sin tema seleccionado"}</h3>
+          <span>
+            {asamblea.turnoAbierto ? (
+              <>
+                <button
+                  type="button"
+                  onClick={() => pedirTurno("Apunte técnico")}
+                >
+                  Apunte técnico
+                </button>
+                <button
+                  type="button"
+                  onClick={() => pedirTurno("Punto de información")}
+                >
+                  Punto de información
+                </button>
+                <button
+                  type="button"
+                  onClick={() => pedirTurno("Respuesta por alusión directa")}
+                >
+                  Respuesta por alusión directa
+                </button>
+                <button
+                  type="button"
+                  onClick={() => pedirTurno("Respuesta normal")}
+                >
+                  Respuesta normal
+                </button>
+                <button
+                  type="button"
+                  onClick={() => pedirTurno("Intervención")}
+                >
+                  Intervención
+                </button>
+              </>
+            ) : (
+              <span>Turno cerrado (womp womp)</span>
+            )}
+          </span>
         </div>
 
         <br />
 
-        <div>
-          <h2>Turnos</h2>
-
-        <div>
-          <span>
-          {asamblea.turnoAbierto ? ( <> 
-          <button type="button" onClick={() => pedirTurno('Apunte técnico')}>Apunte técnico</button>
-          <button type="button" onClick={() => pedirTurno('Punto de información')}>Punto de información</button>
-          <button type="button" onClick={() => pedirTurno('Respuesta por alusión directa')}>Respuesta por alusión directa</button>
-          <button type="button" onClick={() => pedirTurno('Respuesta normal')}>Respuesta normal</button>
-          <button type="button" onClick={() => pedirTurno('Intervención')}>Intervención</button></> ) : ( <span>Turno cerrado (womp womp)</span> )}
-          </span>
-        </div>
-
-        <br/>
-
-          <div>
-            {asamblea.turnos.map((turno, index) => (
-              <div key={turno.id}>
-
-                <div>
-                  <span>{index + 1 + ". "}</span>
-                  <span>{`${turno.nombre} - ${turno.delegacion}: `}</span>
-                  <span>{turno.intervencion}</span>
-                  <span>{turno.nombre === usuario.nombre && turno.delegacion === usuario.delegacion ? <button type="button" onClick={() => cortarTurno(turno)}>Cortar turno</button> : ''}</span>
-                </div>
-
+        <div className="flex flex-col gap-5">
+          {asamblea.turnos.map((turno, index) => (
+            <div className="flex flex-row" key={turno.id}>
+              <div className="flex flex-row gap-5 w-full border border-ceet radius-md">
+                <span className="font-semibold text-gray-600 ">
+                  {index + 1}.
+                </span>
+                <span className="font-semibold text-ceet">
+                  {turno.nombre} - {turno.delegacion}:
+                </span>
+                <span className="text-gray-600">{turno.intervencion}</span>
               </div>
-            ))}
-          </div>
+              <span className="border border-red-800">
+                {turno.nombre === usuario.nombre &&
+                turno.delegacion === usuario.delegacion ? (
+                  <button type="button" onClick={() => cortarTurno(turno)}>
+                    Cortar turno
+                  </button>
+                ) : (
+                  ""
+                )}
+              </span>
+            </div>
+          ))}
         </div>
-        <button type="button" onClick={cerrarSesion}>Cerrar sesión</button>
       </div>
-    );
+      <button
+        className="border border-red-800 radius-md"
+        type="button"
+        onClick={cerrarSesion}
+      >
+        Cerrar sesión
+      </button>
+    </div>
+  );
 }
